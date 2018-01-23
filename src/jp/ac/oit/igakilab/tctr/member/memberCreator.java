98be8b11a2b814666df0c415e9df59e27db6b89a
kjm2086 <2086psp@gmail.com>
2018-01-23 17:49:35 +0900

    1-2-3end
    1-2-3終わりました

--- src/jp/ac/oit/igakilab/tctr/member/memberCreator.java
+++ src/jp/ac/oit/igakilab/tctr/member/memberCreator.java
@@ -8,18 +8,20 @@ import org.apache.commons.lang3.RandomStringUtils;
 import jp.ac.oit.igakilab.tctr.db.MemberDatabaseCreator;
 
 public class memberCreator {
-MemberDatabaseCreator mdc = new MemberDatabaseCreator();
-List<memberManager> mml = new ArrayList<>();
-memberManager mm = new memberManager();
-public void memberInsert(String name,String org,String position,int age){
-mm.setName(name);
-mm.setOrg(org);
-mm.setPosition(position);
-mm.setAge(age);
-mm.setId(RandomStringUtils.randomAlphabetic(1)+RandomStringUtils.randomAlphanumeric(5));
-mdc.memberInsert("memberInsert", mm);
-}
+	MemberDatabaseCreator mdc = new MemberDatabaseCreator();
+	List<memberManager> mml = new ArrayList<>();
+	memberManager mm = new memberManager();
+	public void memberInsert(String name,String org,String position,int age){
+		mm.setName(name);
+		mm.setOrg(org);
+		mm.setPosition(position);
+		mm.setAge(age);
+		mm.setId(RandomStringUtils.randomAlphabetic(1)+RandomStringUtils.randomAlphanumeric(5));
+		mdc.memberInsert("memberInsert", mm);
+	}
 
-public static void main(String[] args){
-}
+	public static void main(String[] args){
+		memberCreator mc = new memberCreator();
+		mc.memberInsert("田中", "igklab", "4回生", 22);
+	}
 }
