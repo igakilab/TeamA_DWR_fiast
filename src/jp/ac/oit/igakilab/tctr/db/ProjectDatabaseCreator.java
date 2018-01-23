ea85d43785b958d19ee7cb0756feaf9103db58f5
ryoji7583 <ryoji.75830923@gmail.com>
2018-01-23 17:31:25 +0900

    値を追加するための汎用DBメソッドの作成
    値を追加するための汎用DBメソッドの作成した

--- src/jp/ac/oit/igakilab/tctr/db/ProjectDatabaseCreator.java
+++ src/jp/ac/oit/igakilab/tctr/db/ProjectDatabaseCreator.java
@@ -9,15 +9,21 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import jp.ac.oit.igakilab.tctr.project.projectManager;
 
 public class ProjectDatabaseCreator {
-SqlSessionFactory factory = DBUtility.getSqlSessionFactory();
-List<projectManager> selectList = new ArrayList<>();
+	SqlSessionFactory factory = DBUtility.getSqlSessionFactory();
+	List<projectManager> selectList = new ArrayList<>();
 
 
 
-public List<projectManager> projectListen(String mapper){
-try (SqlSession session = factory.openSession()){
-selectList = session.selectList(mapper);
-}
-return selectList;
-}
+	public List<projectManager> projectListen(String mapper){
+		try (SqlSession session = factory.openSession()){
+			selectList = session.selectList(mapper);
+		}
+		return selectList;
+	}
+	public void projectInsert(String mapper,projectManager pm){
+		try (SqlSession session = factory.openSession()){
+			session.insert(mapper,pm);
+			session.commit();
+		}
+	}
 }
